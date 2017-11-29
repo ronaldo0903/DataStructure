d445113c490f0f3990514c1edfb9a5fac0c3190a
EYEYTAN <henry.tan@ericsson.com>
2017-11-29 16:49:26 +0800

    Finish main APIs and UT for BT

--- src/titi/Learning/ClassicDatastructure/Tree/BinaryTree.java
+++ src/titi/Learning/ClassicDatastructure/Tree/BinaryTree.java
@@ -12,156 +12,206 @@ package titi.Learning.ClassicDatastructure.Tree;
 import java.util.ArrayDeque;
 
 public class BinaryTree<K extends Comparable<K>, V> {
-	private BinaryTreeNode<K, V> root = null;
+    private BinaryTreeNode<K, V> root = null;
 
-	public BinaryTree(BinaryTreeNode<K, V> node) {
-		root = node;
+    public BinaryTree(BinaryTreeNode<K, V> node) {
+	root = node;
+    }
+
+    public boolean IsEmpty() {
+	return root == null;
+    }
+
+    public int Height() {
+	return height(root);
+    }
+
+    public int Size() {
+	return size(root);
+    }
+
+    public void Destroy() {
+	destroy(root);
+    }
+
+    public BinaryTreeNode<K, V> getRoot() {
+	return root;
+    }
+
+    // 前序遍历(递归实现)
+    public void PreOrder(BinaryTreeNode<K, V> node) {
+	if (node != null) {
+	    visit(node);
+	    PreOrder(node.getLeft());
+	    PreOrder(node.getRight());
 	}
+    }
 
-	public boolean IsEmpty() {
-		return root == null;
+    // 中序遍历(递归实现)
+    public void InOrder(BinaryTreeNode<K, V> node) {
+	if (node != null) {
+	    InOrder(node.getLeft());
+	    visit(node);
+	    InOrder(node.getRight());
 	}
+    }
 
-	public int Height() {
-		return height(root);
+    // 后序遍历(递归实现)
+    public void PostOrder(BinaryTreeNode<K, V> node) {
+	if (node != null) {
+	    PostOrder(node.getLeft());
+	    PostOrder(node.getRight());
+	    visit(node);
 	}
+    }
 
-	public int Size() {
-		return size(root);
-	}
-
-	public void Destroy() {
-		destroy(root);
-	}
-
-	// 前序遍历(递归实现)
-	public void PreOrder(BinaryTreeNode<K, V> node) {
-		if (node != null) {
-			visit(node);
-			PreOrder(node.getLeft());
-			PreOrder(node.getRight());
+    // 层次遍历
+    // 这里以队列的方式使用ArrayDeque
+    public void HierarchicalOrder(BinaryTreeNode<K, V> node) {
+	ArrayDeque<BinaryTreeNode<K, V>> nodesQueue = new ArrayDeque<BinaryTreeNode<K, V>>();
+	if (node != null) {
+	    nodesQueue.addLast(node);
+	    while (!nodesQueue.isEmpty()) {
+		BinaryTreeNode<K, V> curNode = nodesQueue.pollFirst();
+		visit(curNode);
+		if (curNode.getLeft() != null) {
+		    nodesQueue.addLast(curNode.getLeft());
 		}
+		if (curNode.getRight() != null) {
+		    nodesQueue.addLast(curNode.getRight());
+		}
+	    }
 	}
+    }
 
-	// 中序遍历(递归实现)
-	public void InOrder(BinaryTreeNode<K, V> node) {
-		if (node != null) {
-			InOrder(node.getLeft());
-			visit(node);
-			InOrder(node.getRight());
-		}
+    // 前序遍历的非递归实现
+    public void PreOrderNonRecursive(BinaryTreeNode<K, V> node) {
+	ArrayDeque<BinaryTreeNode<K, V>> nodesStack = new ArrayDeque<BinaryTreeNode<K, V>>();
+	while ((node != null) || !nodesStack.isEmpty()) {
+	    while (node != null) {
+		visit(node);
+		nodesStack.push(node);
+		node = node.getLeft();
+	    }
+	    if (!nodesStack.isEmpty()) {
+		node = nodesStack.pop();
+		node = node.getRight();
+	    }
 	}
+    }
 
-	// 后序遍历(递归实现)
-	public void PostOrder(BinaryTreeNode<K, V> node) {
-		if (node != null) {
-			PostOrder(node.getLeft());
-			PostOrder(node.getRight());
-			visit(node);
-		}
+    // 中序遍历的非递归实现
+    public void InOrderNonRecursive(BinaryTreeNode<K, V> node) {
+	ArrayDeque<BinaryTreeNode<K, V>> nodesStack = new ArrayDeque<BinaryTreeNode<K, V>>();
+	while ((node != null) || !nodesStack.isEmpty()) {
+	    while (node != null) {
+		nodesStack.push(node);
+		node = node.getLeft();
+	    }
+	    if (!nodesStack.isEmpty()) {
+		node = nodesStack.pop();
+		visit(node);
+		node = node.getRight();
+	    }
 	}
+    }
 
-	// 层次遍历
-	// 这里以队列的方式使用ArrayDeque
-	public void HierarchicalOrder(BinaryTreeNode<K, V> node) {
-		ArrayDeque<BinaryTreeNode<K, V>> nodesQueue = new ArrayDeque<BinaryTreeNode<K, V>>();
-		if (node != null) {
-			nodesQueue.addLast(node);
-			while (!nodesQueue.isEmpty()) {
-				BinaryTreeNode<K, V> curNode = nodesQueue.pollFirst();
-				visit(curNode);
-				nodesQueue.addLast(curNode.getLeft());
-				nodesQueue.addLast(curNode.getRight());
-			}
+    // 后序遍历的非递归实现(双栈实现)
+    public void PostOrderNonRecursive_DoubleStack(BinaryTreeNode<K, V> node) {
+	ArrayDeque<BinaryTreeNode<K, V>> helpStack = new ArrayDeque<BinaryTreeNode<K, V>>();
+	ArrayDeque<BinaryTreeNode<K, V>> nodesStack = new ArrayDeque<BinaryTreeNode<K, V>>();
+	while ((node != null) || !helpStack.isEmpty()) {
+	    while (node != null) {
+		helpStack.push(node);
+		nodesStack.push(node);
+		node = node.getRight();
+	    }
+	    if (!helpStack.isEmpty()) {
+		node = helpStack.pop();
+		while ((node.getLeft() == null) && !helpStack.isEmpty()) {
+		    node = helpStack.pop();
 		}
+	    }
+	    node = node.getLeft();
 	}
+	while (!nodesStack.isEmpty()) {
+	    visit(nodesStack.pop());
+	}
+    }
 
-	// 前序遍历的非递归实现
-	public void PreOrderNonRecursive(BinaryTreeNode<K, V> node) {
-		ArrayDeque<BinaryTreeNode<K, V>> nodesStack = new ArrayDeque<BinaryTreeNode<K, V>>();
-		while ((node != null) || !nodesStack.isEmpty()) {
-			while (node != null) {
-				visit(node);
-				nodesStack.push(node);
-				node = node.getLeft();
-			}
-			if (!nodesStack.isEmpty()) {
-				node = nodesStack.pop();
-				node = node.getRight();
-			}
+    // 后序遍历的非递归实现(栈实现)
+    public void PostOrderNonRecursive_SingleStack(BinaryTreeNode<K, V> node) {
+	ArrayDeque<BinaryTreeNode<K, V>> nodesStack = new ArrayDeque<BinaryTreeNode<K, V>>();
+	BinaryTreeNode<K, V> pNode = node;
+	while (node != null) {
+	    for (; node.getLeft() != null; node = node.getLeft()) {
+		nodesStack.push(node);
+	    }
+	    while ((node != null) && ((node.getRight() == null) || (node.getRight() == pNode))) {
+		visit(node);
+		pNode = node;
+		if (nodesStack.isEmpty()) {
+		    return;
 		}
+		node = nodesStack.pop();
+	    }
+	    nodesStack.push(node);
+	    node = node.getRight();
 	}
+    }
 
-	// 中序遍历的非递归实现
-	public void InOrderNonRecursive(BinaryTreeNode<K, V> node) {
-		ArrayDeque<BinaryTreeNode<K, V>> nodesStack = new ArrayDeque<BinaryTreeNode<K, V>>();
-		while ((node != null) || !nodesStack.isEmpty()) {
-			while (node != null) {
-				nodesStack.push(node);
-				node = node.getLeft();
-			}
-			if (!nodesStack.isEmpty()) {
-				node = nodesStack.pop();
-				visit(node);
-				node = node.getRight();
-			}
-		}
+    public BinaryTreeNode<K, V> ParentNode(BinaryTreeNode<K, V> node) {
+	if ((null == root) || (node == root)) {
+	    return null;
 	}
+	return parent(root, node);
+    }
 
-	// 后序遍历的非递归实现(双栈实现)
-	public void PostOrderNonRecursive(BinaryTreeNode<K, V> node) {
-		ArrayDeque<BinaryTreeNode<K, V>> helpStack = new ArrayDeque<BinaryTreeNode<K, V>>();
-		ArrayDeque<BinaryTreeNode<K, V>> nodesStack = new ArrayDeque<BinaryTreeNode<K, V>>();
-		while ((node != null) || !nodesStack.isEmpty()) {
-			while (node != null) {
-				helpStack.push(node);
-				nodesStack.push(node);
-				node = node.getRight();			
-			}
-			if (!helpStack.isEmpty()) {
-				node = helpStack.pop();
-				while(node.getLeft() == null) {
-					node = helpStack.pop();
-				}
-				node = node.getLeft();			
-			}
-		}
-		while(!nodesStack.isEmpty()) {
-			visit(nodesStack.pop());
-		}
+    private BinaryTreeNode<K, V> parent(BinaryTreeNode<K, V> subTreeRoot, BinaryTreeNode<K, V> node) {
+	if (subTreeRoot == null) {
+	    return null;
 	}
+	if ((node == subTreeRoot.getLeft()) || (node == subTreeRoot.getRight())) {
+	    return subTreeRoot;
+	}
+	BinaryTreeNode<K, V> pNode;
+	if ((pNode = parent(subTreeRoot.getLeft(), node)) != null) {
+	    return pNode;
+	}
+	return parent(subTreeRoot.getRight(), node);
+    }
 
-	private void destroy(BinaryTreeNode<K, V> tree) {
-		if (tree != null) {
-			destroy(tree.getLeft());
-			destroy(tree.getRight());
-			tree = null;
-		}
+    private void destroy(BinaryTreeNode<K, V> tree) {
+	if (tree != null) {
+	    destroy(tree.getLeft());
+	    destroy(tree.getRight());
+	    tree = null;
 	}
+    }
 
-	private int size(BinaryTreeNode<K, V> tree) {
-		if (tree == null) {
-			return 0;
-		} else {
-			return 1 + size(tree.getLeft()) + size(tree.getRight());
-		}
+    private int size(BinaryTreeNode<K, V> tree) {
+	if (tree == null) {
+	    return 0;
+	} else {
+	    return 1 + size(tree.getLeft()) + size(tree.getRight());
 	}
+    }
 
-	private int height(BinaryTreeNode<K, V> tree) {
-		if (tree == null) {
-			return 0;
-		} else {
-			int leftHeight = height(tree.getLeft());
-			int rightHeight = height(tree.getRight());
-			return (leftHeight < rightHeight) ? (rightHeight + 1) : (leftHeight + 1);
-		}
+    private int height(BinaryTreeNode<K, V> tree) {
+	if (tree == null) {
+	    return 0;
+	} else {
+	    int leftHeight = height(tree.getLeft());
+	    int rightHeight = height(tree.getRight());
+	    return (leftHeight < rightHeight) ? (rightHeight + 1) : (leftHeight + 1);
 	}
+    }
 
-	private void visit(BinaryTreeNode<K, V> node) {
-		if (node != null) {
-			node.setVisited(true);
-			System.out.println("Now visiting node with key:" + node.getKey().toString() + ", value:"
-					+ node.getData().toString());
-		}
+    private void visit(BinaryTreeNode<K, V> node) {
+	if (node != null) {
+	    node.setVisited(true);
+	    System.out.println("Now visiting node with key:" + node.getKey().toString() + ", value:"
+		    + node.getData().toString());
 	}
+    }
 }
--- src/titi/Learning/ClassicDatastructure/Tree/TreeTest.java
+++ src/titi/Learning/ClassicDatastructure/Tree/TreeTest.java
@@ -1,62 +1,87 @@
 package titi.Learning.ClassicDatastructure.Tree;
 
-import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import junit.framework.Assert;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class TreeTest {
-	private static List<BinaryTreeNode<Integer,String>> treeNodeList = new ArrayList();
-	private static BinaryTree<Integer,String> root;
-	
-	/*
-	 * 构造如下结构的二叉树
-	 * 			  1
-	 * 		2   	    3
-	 	4		5     9   
-	 	  8	   6    10  11
-	*           7          12
-	*/
-	@BeforeClass
-	public static void buildTree() {
-		for(int i=1; i<=12; i++) {
-			treeNodeList.add(i, new BinaryTreeNode(i, String.valueOf(i)));
-		}
-		treeNodeList.get(1).setLeft(treeNodeList.get(2));
-		treeNodeList.get(1).setRight(treeNodeList.get(3));
-		treeNodeList.get(2).setLeft(treeNodeList.get(4));
-		treeNodeList.get(2).setRight(treeNodeList.get(5));
-		treeNodeList.get(3).setLeft(treeNodeList.get(9));
-		treeNodeList.get(4).setRight(treeNodeList.get(8));
-		treeNodeList.get(5).setLeft(treeNodeList.get(6));
-		treeNodeList.get(6).setRight(treeNodeList.get(7));
-		treeNodeList.get(9).setLeft(treeNodeList.get(10));
-		treeNodeList.get(9).setRight(treeNodeList.get(11));
-		treeNodeList.get(11).setRight(treeNodeList.get(12));
-		root = new BinaryTree(treeNodeList.get(1));
-	}
+    private static List<BinaryTreeNode<Integer, String>> treeNodeList = new ArrayList();
+    private static BinaryTree<Integer, String> tree;
 
-	@Test
-	public void testPreOrder() {
-		
+    /*
+     * 构造如下结构的二叉树: 
+     * <pre>
+     *		      1
+     *  	2   	       3
+     * 	   4	      5      9   
+     *       8	   6      10   11
+     *                7           12 
+     * 
+     */
+    @BeforeClass
+    public static void buildTree() {
+	for (int i = 0; i <= 12; i++) {
+	    treeNodeList.add(i, new BinaryTreeNode(i, String.valueOf(i)));
 	}
-	
-	@Test
-	public void testPostOrder() {
-		
-	}
-	
-	@Test
-	public void testInOrder() {
-		
-	}
-	
-	@Test
-	public void testHierarchicalOrder() {
-		
-	}
+	treeNodeList.get(1).setLeft(treeNodeList.get(2));
+	treeNodeList.get(1).setRight(treeNodeList.get(3));
+	treeNodeList.get(2).setLeft(treeNodeList.get(4));
+	treeNodeList.get(2).setRight(treeNodeList.get(5));
+	treeNodeList.get(3).setLeft(treeNodeList.get(9));
+	treeNodeList.get(4).setRight(treeNodeList.get(8));
+	treeNodeList.get(5).setLeft(treeNodeList.get(6));
+	treeNodeList.get(6).setRight(treeNodeList.get(7));
+	treeNodeList.get(9).setLeft(treeNodeList.get(10));
+	treeNodeList.get(9).setRight(treeNodeList.get(11));
+	treeNodeList.get(11).setRight(treeNodeList.get(12));
+	tree = new BinaryTree(treeNodeList.get(1));
+    }
+
+    @Test
+    public void testPreOrder() {
+	System.out.println("PreOrder in Recursive way:");
+	tree.PreOrder(tree.getRoot());
+	System.out.println("==================");
+	System.out.println("PreOrder in Non-Recursive way:");
+	tree.PreOrderNonRecursive(tree.getRoot());
+    }
+
+    @Test
+    public void testPostOrder() {
+	System.out.println("PostOrder in Recursive way:");
+	tree.PostOrder(tree.getRoot());
+	System.out.println("==================");
+	System.out.println("PostOrder in Non-Recursive way using double stack:");
+	tree.PostOrderNonRecursive_DoubleStack(tree.getRoot());
+	System.out.println("==================");
+	System.out.println("PostOrder in Non-Recursive way using single stack:");
+	tree.PostOrderNonRecursive_SingleStack(treeNodeList.get(2));
+    }
+
+    @Test
+    public void testInOrder() {
+	System.out.println("InOrder in Recursive way:");
+	tree.InOrder(tree.getRoot());
+	System.out.println("==================");
+	System.out.println("InOrder in Non-Recursive way:");
+	tree.InOrderNonRecursive(tree.getRoot());
+    }
+
+    @Test
+    public void testHierarchicalOrder() {
+	System.out.println("Traverse tree in Hierarchical Order:");
+	tree.HierarchicalOrder(tree.getRoot());
+    }
+
+    @Test
+    public void testParent() {
+	Assert.assertEquals(tree.ParentNode(treeNodeList.get(6)).getKey().intValue(), 5);
+	Assert.assertEquals(tree.ParentNode(treeNodeList.get(7)).getKey().intValue(), 6);
+	Assert.assertEquals(tree.ParentNode(treeNodeList.get(9)).getKey().intValue(), 3);
+    }
 
 }
