5e1e3862e40eb609a41967c3e1bc2d5ad16c764c
TiTi <ronaldo0903@126.com>
2019-05-25 22:21:53 +0800

    Add test for thread lifecycle

--- src/titi/Learning/java/io/TCPServer.java
+++ src/titi/Learning/java/io/TCPServer.java
@@ -9,14 +9,16 @@ public class TCPServer {
         //服务端在20006端口监听客户端请求的TCP连接  
         ServerSocket server = new ServerSocket(20006);  
         Socket client = null;  
-        boolean f = true;  
+        boolean f = true;
+        int connectClients = 0;
         while(f){  
             //等待客户端的连接，如果没有获取连接  
-            client = server.accept();  
-            System.out.println("与客户端连接成功！");  
+            client = server.accept();
+            connectClients ++;
+            System.out.println("与第" + connectClients + "个客户端连接成功！");
             //为每个客户端连接开启一个线程  
-            //new Thread(new TCPServerThread(client)).start();
-            new TCPServerThread(client).run();
+            new Thread(new TCPServerThread(client), "TCPClient#" + connectClients).start();
+            //new TCPServerThread(client).run();
         }  
         server.close();  
     }  
---
+++ src/titi/Learning/java/thread/ThreadLifeCycleTest.java
@@ -0,0 +1,74 @@
+package titi.Learning.java.thread;
+
+import java.util.concurrent.TimeUnit;
+/*
+1. Java中所谓的父子进程间并没有生命周期上的绑定关系，父进程结束后子进程可以继续执行；
+2. 守护进程会在所有用户线程都执行完毕后自动终止；
+3. Java的主线程执行结束并不意味着JVM进程退出，其它线程还可能在继续运行；
+
+ */
+public class ThreadLifeCycleTest {
+    public static void main(String[] args)
+    {
+        Thread mainThread = new Thread(new Runnable(){
+            @Override
+            public void run()
+            {
+                Thread childThread = new Thread(new ChildThread(), "My Child Thread");
+                childThread.setDaemon(true);
+                childThread.start();
+                System.out.println("I'm main thread...");
+            }
+        }, "My Main Thread");
+        mainThread.start();
+
+        Thread otherThread = new Thread(new Runnable(){
+            @Override
+            public void run()
+            {
+                while(true)
+                {
+                    System.out.println("I'm other user thread...");
+                    try
+                    {
+                        TimeUnit.MILLISECONDS.sleep(1000);
+                    }
+                    catch (InterruptedException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, "Other Thread");
+        otherThread.start();
+        try {
+            //Thread.currentThread().stop();
+            Thread.sleep(10000);
+            mainThread.join(10000);
+            System.out.println("Java Main thread exits now!");
+            //otherThread.join(10000);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
+
+class ChildThread implements Runnable
+{
+    @Override
+    public void run()
+    {
+        while(true)
+        {
+            System.out.println("I'm child thread..");
+            try
+            {
+                TimeUnit.MILLISECONDS.sleep(1000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
